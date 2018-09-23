7b833bf9eb18ce38f34615dce3545dde080eced5
humraz <humrazsait@gmail.com>
2018-09-23 11:43:15 +0530

    first

---
+++ app/src/main/java/org/tensorflow/demo/Constants.java
@@ -0,0 +1,10 @@
+package org.tensorflow.demo;
+
+/**
+ * Created by humraz on 2/14/18.
+ */
+
+    public class Constants {
+        public static final String UPLOAD_URL = "http://172.20.10.7:8000/imageess/";
+        public static final String IMAGES_URL = "http://192.168.94.1/AndroidImageUpload/getImages.php";
+    }
